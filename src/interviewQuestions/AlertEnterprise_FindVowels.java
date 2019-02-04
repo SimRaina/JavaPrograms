168f7f30ae241235bd8ec92ff80b45eab2a0c517
SimranRai22 <simran.pre22@gmail.com>
2019-02-04 11:23:58 +0530

    Added 1 question from Second Skype Interview from Alert Enterprise Chandigarh

---
+++ src/interviewQuestions/AlertEnterprise_FindVowels.java
@@ -0,0 +1,55 @@
+package interviewQuestions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class AlertEnterprise_FindVowels {
+
+		public static void main(String[] args) {
+			
+			
+			String str="ALERTENTERPRISE";
+			
+	      findVowels(str);
+		}
+		
+		static void findVowels(String s)
+		{
+			
+			Map<Character,Integer> map= new HashMap<>();
+			ArrayList<Integer> list=new ArrayList<>();
+			//Character ch[]=s.toCharArray();   
+			
+			for (int i=0;i<s.length();i++) {
+				
+				Character c=s.charAt(i);
+				
+				if((c.equals('A')) || (c.equals('E')) || (c.equals('I')) || (c.equals('O')) || (c.equals('U'))) {
+				// Tricky Part: equals can work on Character and not primitive type char
+					// so created Character ch but toCharArray cannot be invoked on Character
+					// so used charAt()
+					list.add(i);
+				if(!(map.containsKey(c))) {
+					map.put(c, 1);
+				}
+				else 
+					map.put(c, map.get(c)+1);
+			}
+		}
+		
+		Set<Entry<Character,Integer>> entryset=map.entrySet();	
+			
+			for (Entry<Character,Integer> entry: entryset){
+				
+				System.out.println("Character:" + entry.getKey() + " occured in the string: " + entry.getValue() + " times" );
+			}
+			for (int i = 0; i<list.size();i++) {
+				System.out.println("Location of " + i + " vowel " + list.get(i));
+			}
+		}
+
+	}
+
