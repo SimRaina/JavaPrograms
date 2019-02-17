aae1b49fef1cc81974f2a8503b431fd610bcc876
SimranRai22 <simran.pre22@gmail.com>
2019-02-17 20:04:30 +0530

    Added interview question related to Skype interview with TestingXperts Chandigarh

---
+++ src/interviewQuestions/TestingXperts_ReverseSentence.java
@@ -0,0 +1,25 @@
+package interviewQuestions;
+
+
+public class TestingXperts_ReverseSentence {
+
+	public static void main(String[] args) {
+		String str="This is my name";
+		
+		System.out.println("Reverse is=> " + findreverse(str));
+
+	}
+	
+	static String findreverse(String s) {
+		
+		 String res="";
+		 String[] words = s.split(" "); 
+		 for (int i = words.length-1; i >= 0; i--) { 
+		 res = res + words[i] + " "; 
+		
+	}
+		    return String.join(" ", res);
+
+}
+}
+
