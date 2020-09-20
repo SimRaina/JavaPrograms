6799a93f643da5ab7cfda1c1eee4c5294e6fd18f
SimranRaina <simran.pre22@gmail.com>
2020-09-20 16:04:20 +0530

    Added Fibonacci folder and recursive approach

---
+++ src/fibonacci/Fibo_Recur.java
@@ -0,0 +1,33 @@
+package fibonacci;
+
+import java.util.Scanner;
+
+public class Fibo_Recur {
+    
+	static int result;
+	public static void main(String[] args) {
+
+	  Scanner sc = new Scanner(System.in);
+	  System.out.print("Enter Fibonacci Series position: ");
+	  int n = sc.nextInt();
+	  
+      System.out.println("Value at " + n + "th position in the series is: " + fib(n));
+      sc.close();
+
+	}
+	
+	static int fib(int i) {
+	
+		
+		if (i==1 | i==2) {
+			result= 1;
+		}
+		
+		else {
+			result = fib(i-1) + fib(i-2);
+		}
+		
+		return result;
+	}
+
+}
