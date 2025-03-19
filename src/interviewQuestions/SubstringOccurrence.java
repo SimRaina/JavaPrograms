0f53fd8e63151d3554d90bce5681cd04ccfabef8
Sim Raina <simran.pre90@gmail.com>
2025-03-19 22:52:22 +0530

    Update SubstringOccurrence.java

--- src/interviewQuestions/SubstringOccurrence.java
+++ src/interviewQuestions/SubstringOccurrence.java
@@ -4,29 +4,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class SubstringOccurrence {
-
 	public static void main(String[] args) {
 		String str="A man, a plan, a canal, Panama";
 		//logic1(str);
 		logic2(str);
 	}
-	
 	static void logic1(String str) {
         int count=0;
-		
 		String words[] = str.split(" ");
-		
 		for(int i=0; i<words.length; i++) {
-			
-			
 			if(words[i].length() > 2) {
-				
 				if(words[i].contains("an")) {
 					count++;
 				}	
 			}
 		}
-		
 		System.out.println(" \"an\" exists: " + count + " times");
 	}
     
