1d33b02c65d4d73afdde0012aaf115554ab62262
Sim Raina <simran.pre90@gmail.com>
2025-03-19 22:29:23 +0530

    Update FindYoungest.java

--- src/interviewQuestions/FindYoungest.java
+++ src/interviewQuestions/FindYoungest.java
@@ -6,32 +6,30 @@ import java.util.Map.Entry;
 
 public class FindYoungest {
 
-		public static void main(String[] args) {
-			
-		  String str="ALERTENTERPRISE";
-		  findVowels(str);
-		}
-		
-		static void findVowels(String s)
-		{
-			
-			Map<Character,Integer> map= new HashMap<>();
-			
-			for (int i=0;i<s.length();i++) {
-				
-				Character c=s.charAt(i);
-				
-				if((c.equals('A')) || (c.equals('E')) || (c.equals('I')) || (c.equals('O')) || (c.equals('U'))) {
-				/* equals can work on Character and not primitive type char
-				so created Character ch but toCharArray cannot be invoked on Character so used charAt()*/	
-				map.put(c, map.getOrDefault(c,0)+1);
-			}
-		}
-			
-			for (Entry<Character,Integer> entry: map.entrySet()){
-				
-				System.out.println("Character: " + entry.getKey() + " occured in the string: " + entry.getValue() + " times" );
-			}
-		}
-	}
+	public static void main(String[] args) {
+		// Creating a HashMap to store student names and their ages
+        HashMap<String, Integer> students = new HashMap<>();
+        
+        // Adding student data
+        students.put("Alice", 20);
+        students.put("Bob", 19);
+        students.put("Charlie", 22);
+        students.put("David", 18);
+        students.put("Emma", 21);
+
+        // Finding the youngest student
+        String youngestStudent = null;
+        int minAge = Integer.MAX_VALUE;
+
+        for (Map.Entry<String, Integer> entry : students.entrySet()) {
+            if (entry.getValue() < minAge) {
+                minAge = entry.getValue();
+                youngestStudent = entry.getKey();
+            }
+        }
+
+        // Printing the youngest student name
+        System.out.println("The youngest student is: " + youngestStudent + " (Age: " + minAge + ")");
+    }	
+}
 
