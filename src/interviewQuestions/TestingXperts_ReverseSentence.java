8bb5def3d4cf31f97edab6ddec24bbe6fed5e4bb
Sim Raina <simran.pre90@gmail.com>
2025-03-19 22:58:20 +0530

    Update TestingXperts_ReverseSentence.java

--- src/interviewQuestions/TestingXperts_ReverseSentence.java
+++ src/interviewQuestions/TestingXperts_ReverseSentence.java
@@ -2,16 +2,11 @@ package interviewQuestions;
 
 
 public class TestingXperts_ReverseSentence {
-
 	public static void main(String[] args) {
 		String str="This is my name";
-		
 		System.out.println("Reverse is=> " + findreverse(str));
-
 	}
-	
 	static String findreverse(String s) {
-		
 		 String res="";
 		 String[] words = s.split(" "); 
 		 for (int i = words.length-1; i >= 0; i--) { 
@@ -20,4 +15,3 @@ public class TestingXperts_ReverseSentence {
 		 return String.join(" ", res);
 	}
 }
-
