7020992084ec72367b74df9e2c79ad9ac27fa9b2
Sim Raina <simran.pre90@gmail.com>
2025-03-19 22:45:26 +0530

    Update RemoveDuplicateInString.java

--- src/interviewQuestions/RemoveDuplicateInString.java
+++ src/interviewQuestions/RemoveDuplicateInString.java
@@ -3,28 +3,19 @@ package interviewQuestions;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * @author Simran
- *
- */
 public class RemoveDuplicateInString {
-
-	
 	public static void main(String[] args) {
-	String str="Preet";
-	System.out.println("Length of given String is:"+str.length());
-	removeDuplicates(str);
-	}
-	 
+		String str="Preet";
+		System.out.println("Length of given String is:"+str.length());
+		removeDuplicates(str);
+		}
 	public static void removeDuplicates(String str) {
 		Set<Character> set=new HashSet<>();
-		
 		for(int i=0;i<str.length();i++) {
-		char c=str.charAt(i);  
-		set.add(c); // set doesn't contain duplicate
-	}
-
+			char c=str.charAt(i);  
+			set.add(c); // set doesn't contain duplicate
+	        }
 	System.out.println("Size of String with non repeating charcters:" + set.size());	
-}
+        }
 }
 
