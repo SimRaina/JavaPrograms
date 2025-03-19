cb1096a963815f292a36296eff775ae40a0e5cba
Sim Raina <simran.pre90@gmail.com>
2025-03-19 23:01:36 +0530

    Update SecondHighest.java

--- src/array/SecondHighest.java
+++ src/array/SecondHighest.java
@@ -3,10 +3,11 @@ package array;
 public class SecondHighest {
 	
 	static int arr[] = {12, 34, 10, 6, 40}; 
-	      
-
-	    static int secondHighest() 
-	    { 
+        public static void main(String[] args){ 
+	        System.out.println("Second Highest is: " + secondHighest());           
+	 } 
+	
+	static int secondHighest() { 
 	        // Initialize first and second largest element 
 	        int first, second; 
 	        if (arr[0] > arr[1]) 
@@ -19,8 +20,6 @@ public class SecondHighest {
 	            first = arr[1]; 
 	            second = arr[0]; 
 	        } 
-	       
-	 
 	        for (int i = 2; i<arr.length; i++) 
 	        { 
 	            /* If current element is greater than first then update both 
@@ -36,13 +35,6 @@ public class SecondHighest {
 	                second = arr[i]; 
 	        } 
 	        return (second); 
-	    } 
-
-	    public static void main(String[] args)  
-	    { 
-	          
-	        System.out.println("Second Highest is " + secondHighest()); 
-	          
 	    } 
 	} 
 
