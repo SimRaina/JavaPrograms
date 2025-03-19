ec44e46402db57155b645624a6b090ab8140da18
Sim Raina <simran.pre90@gmail.com>
2025-03-19 22:57:20 +0530

    Update SwapFirstLastCharacters.java

--- src/interviewQuestions/SwapFirstLastCharacters.java
+++ src/interviewQuestions/SwapFirstLastCharacters.java
@@ -5,29 +5,21 @@ package interviewQuestions;
  *
  */
 public class SwapFirstLastCharacters {
-
-	
 	public static void main(String[] args) {
 		String str="it is java program";
 		System.out.println(count(str));
 	}
-	
 	static String count(String str) {
 		char[] ch=str.toCharArray();
-		
 		for (int i=0;i<ch.length;i++) {
-			
 			int k=i;
-			
 			while (i<ch.length && ch[i]!=' ') {
 				i++;
 			}
-			
 			char temp=ch[k];
 			ch[k]=ch[i-1]; // i-1 = last character
 			ch[i-1]=temp;
 		}
 		return new String(ch);
 	}
-
 }
