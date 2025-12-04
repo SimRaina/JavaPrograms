ac0c114f1067f95f8425113078f836c9c5a33211
Sim Raina QA <simran.pre90@gmail.com>
2025-12-04 13:52:55 +0100

    optimized as per Java Streams

--- src/interviewQuestions/MoveZeroes.java
+++ src/interviewQuestions/MoveZeroes.java
@@ -1,12 +1,16 @@
 package interviewQuestions;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class MoveZeroes {
 	public static void main(String[] args) {
 		int[] arr = {4, 0, 2, 0, 7, 1, 0, 6, 8};
 		moveZeroes(arr);
+        // moveZeroes1(arr);
 	 }
 	static void moveZeroes(int[] arr1) {
-		 int index = 0;
+        int index = 0;
 		 int n = arr1.length;
 		 
 		 for(int i = 0; i < n; i++) {
@@ -16,10 +20,20 @@ public class MoveZeroes {
 				 arr1[i] = temp;
 				 index++;
 			 }	 
-		  }	    
-                   for(int i=0; i<n; i++) {
-    	                System.out.print(arr1[i]);
-                  }
+		  }
+         for(int i=0; i<n; i++) {
+             System.out.print(arr1[i]);
+         }
       }
+
+    static void moveZeroes1(int[] arr1) {
+        int[] result =
+                IntStream.concat(
+                        Arrays.stream(arr1).filter(x -> x != 0),   // all non-zero values
+                        Arrays.stream(arr1).filter(x -> x == 0)    // all zeros at the end
+                ).toArray();
+
+        System.out.println(Arrays.toString(result));
+    }
 }
 
