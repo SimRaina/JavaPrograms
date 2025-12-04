a00e35c1c850ea2de89edbfc6699a00e63849eca
Sim Raina QA <simran.pre90@gmail.com>
2025-12-04 13:48:35 +0100

    optimized min value and missing number programs with Java Streams

--- src/interviewQuestions/MinimumValue.java
+++ src/interviewQuestions/MinimumValue.java
@@ -1,22 +1,27 @@
 package interviewQuestions;
 
+import java.util.Arrays;
+
 public class MinimumValue {
 
 	public static void main(String[] args) {
-			int input[]= {4,2,1,6,0,8,7};
+			int[] input= {4,2,1,6,0,8,7};
 			System.out.println(min(input));
+            // System.out.println(min1(input));
 	}
 	
 	public static int min(int[] arr) {
-		
+        int[] input = {4,2,1,6,0,8,7};
 		int min=arr[0];
-		
-		for (int i=0;i<arr.length;i++) {
-			
-			if(arr[i] < min) {
-				min=arr[i];
-			}
-		}
+        for (int j : arr) {
+            if (j < min) {
+                min = j;
+            }
+        }
 		return min;
 	}
+
+    public static int min1(int[] arr) {
+        return Arrays.stream(arr).min().orElseThrow();
+    }
 }
--- src/interviewQuestions/MissingNumberArray.java
+++ src/interviewQuestions/MissingNumberArray.java
@@ -1,25 +1,29 @@
 package interviewQuestions;
 
+import java.util.Arrays;
+
 /**
  * @author Simran
- *
  */
 public class MissingNumberArray {
-
-	
 	public static void main(String[] args) {
-		int array[]= {1,2,3,4,6};
-		int missingnumber=getMissingNumber(array,6);
-		System.out.println("Missing number is " + missingnumber);
+		int[] array= {1,2,3,4,6};
+		System.out.println("Missing number is " + getMissingNumber(array, 6));
+        // System.out.println("Missing number is " + getMissingNumber1(array, 6));
 	}
 	
-	private static int getMissingNumber(int array[], int n) {
-		int actualsum=0;
-		int expectedsum=n*(n+1)/2;
-		
+	private static int getMissingNumber(int[] array, int n) {
+		int actualsum = 0;
+		int expectedsum = n * (n + 1) / 2;
 		for (int i:array) {
-			actualsum=actualsum+i;
+			actualsum += i;
 		}
-		return expectedsum-actualsum;
-		}
-	}
+		return expectedsum - actualsum;
+    }
+
+    public static int getMissingNumber1(int[] array, int n) {
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = Arrays.stream(array).sum();
+        return expectedSum - actualSum;
+    }
+}
