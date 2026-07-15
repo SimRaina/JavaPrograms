c5a1937aaa9100cf27b7c9eaf78f8a95ab11c914
Sim Raina QA <simran.pre90@gmail.com>
2026-07-15 19:58:15 +0530

    Updated ReverseArray Program

--- src/interviewQuestions/ReverseArray.java
+++ src/interviewQuestions/ReverseArray.java
@@ -3,14 +3,11 @@ package interviewQuestions;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-/* 2  4  6   8   10
-            -2  -1  Index   */
-
-public class ReverseArrayWithoutUsingOtherArray {
+public class ReverseArray {
 	
 	public static void main(String[] args) {
-		// reverseArray();
-        reverseArray1();
+		reverseArray(); // Reverse array without using another array
+        // reverseArray1();
         // reverseArray2();
   }
 
@@ -20,8 +17,8 @@ public class ReverseArrayWithoutUsingOtherArray {
       for(int i=0; i<=size/2; i++) {
           int k=i;
           int temp = arr[k];
-          arr[k] = arr[size - 1 - i]; // -1, -2, ...
-          arr[size-(i+1)] = temp;
+          arr[k] = arr[size -1 -i]; // -1, -2, ...
+          arr[size-i-1] = temp;
       }
 
       for (int j : arr) {
@@ -29,16 +26,7 @@ public class ReverseArrayWithoutUsingOtherArray {
       }
   }
 
-  public static void reverseArray1() {
-      int[] arr = {2,4,6,8,10};
-      int[] reversed = IntStream.range(0, arr.length)
-              .map(i -> arr[arr.length - 1 - i])
-              .toArray();
-
-      System.out.println(Arrays.toString(reversed));
-  }
-
-  public static void reverseArray2() {
+    public static void reverseArray1() {
       int[] arr = {2,4,6,8,10};
       for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
           int temp = arr[i];
@@ -46,5 +34,14 @@ public class ReverseArrayWithoutUsingOtherArray {
           arr[j] = temp;
       }
       System.out.println(Arrays.toString(arr));
+  }
+
+  public static void reverseArray2() {
+      int[] arr = {2,4,6,8,10};
+      int[] reversed = IntStream.range(0, arr.length)
+              .map(i -> arr[arr.length - 1 - i])
+              .toArray();
+
+      System.out.println(Arrays.toString(reversed));
   }
 }
