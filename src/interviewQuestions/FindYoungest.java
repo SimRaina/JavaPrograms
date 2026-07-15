bc99726c2240c52d9d9a8d4f5d4ff6d57a3d474f
Sim Raina QA <simran.pre90@gmail.com>
2026-07-15 19:14:04 +0530

    Added Map.of()

--- src/interviewQuestions/FindYoungest.java
+++ src/interviewQuestions/FindYoungest.java
@@ -1,6 +1,5 @@
 package interviewQuestions;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -13,36 +12,37 @@ public class FindYoungest {
 
     static void test1() {
 		// Creating a HashMap to store student names and their ages
-        HashMap<String, Integer> students = new HashMap<>();
-        // Adding student data
-        students.put("Alice", 20);
-        students.put("Bob", 19);
-        students.put("Charlie", 22);
-        students.put("David", 18);
-        students.put("Emma", 21);
+        Map<String, Integer> students = Map.of(
+            "Alice", 20,
+            "Bob", 19,
+            "Charlie", 22,
+            "David", 18,
+            "Emma", 21
+        );
 
         // Finding the youngest student
         String youngestStudent = null;
         int minAge = Integer.MAX_VALUE;
 
-        for (Map.Entry<String, Integer> entry : students.entrySet()) {
+        for (Entry<String, Integer> entry : students.entrySet()) {
             if (entry.getValue() < minAge) {
                 minAge = entry.getValue();
                 youngestStudent = entry.getKey();
             }
         }
         // Printing the youngest student name
-        System.out.println("The youngest student is: " + youngestStudent + " (Age: " + minAge + ")");
+        System.out.println("The youngest student is: " 
+        + youngestStudent + " (Age: " + minAge + ")");
     }
 
     static void test2() {
         // Creating a HashMap to store student names and their ages
-        Map<String, Integer> students = new HashMap<>();
-        students.put("Alice", 20);
-        students.put("Bob", 19);
-        students.put("Charlie", 22);
-        students.put("David", 18);
-        students.put("Emma", 21);
+        Map<String, Integer> students = Map.of(
+        "Alice", 20,
+        "Bob", 19,
+        "Charlie", 22,
+        "David", 18,
+        "Emma", 21);
 
         // Finding the youngest student using Streams
         students.entrySet()
@@ -50,7 +50,8 @@ public class FindYoungest {
                 .min(Entry.comparingByValue())
                 .ifPresent(youngest ->
                         System.out.println("The youngest student is: "
-                        + youngest.getKey() + " (Age: " + youngest.getValue() + ")"));
+                        + youngest.getKey() 
+                        + " (Age: " + youngest.getValue() + ")"));
     }
 }
 
